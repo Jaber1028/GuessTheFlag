4cd5f2d1917a41b3dd030e4cf07c01a79d65eee1
Jacob Aberasturi <jacobaberasturi@gmail.com>
2023-01-16 22:25:28 -0500

    Added game over when past 8 turns. This allows us to not choose duplicate flags! Also added turn count, and hides the game when the game is over. Additionally, added a alert when the game is over and a button to restart the game! Also, The game tells you now what flag you choose when it was not the correct one

--- ContentView.swift
+++ ContentView.swift
@@ -12,8 +12,12 @@ struct ContentView: View {
     @State private var scoreTitle = ""
     @State private var score = 0
     
-    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
+    @State private var seenSoFarCountries = [String]()
     @State private var correctAnswer = Int.random(in: 0...2)
+    
+    @State private var turnCount = 0
+    @State private var gameOver = false
     var body: some View {
         
         ZStack(alignment: .top) {
@@ -29,33 +33,36 @@ struct ContentView: View {
                 Text("Guess the Flag")
                     .font(.largeTitle.bold())
                     .foregroundColor(.primary)
-                VStack(spacing: 25){
-                    VStack {
-                        Text("Tap the flag of")
-                            .foregroundStyle(.primary)
-                            .font(.subheadline.weight(.heavy))
-                        Text(countries[correctAnswer])
-                            .foregroundStyle(.primary)
-                            .font(.largeTitle.weight(.semibold))
-                    }
-                    
-                    ForEach(0..<3) { number in
-                        Button {
-                            flagTapped(number)
-                        } label: {
-                            Image(countries[number])
-                                .renderingMode(.original)
-                                .clipShape(RoundedRectangle(cornerRadius: 35))
-                                .shadow(radius: 23)
+                if !gameOver {
+                    Text("Turn: \(turnCount + 1) out of 8")
+                    VStack(spacing: 25){
+                        VStack {
+                            Text("Tap the flag of")
+                                .foregroundStyle(.primary)
+                                .font(.subheadline.weight(.heavy))
+                            Text(countries[correctAnswer])
+                                .foregroundStyle(.primary)
+                                .font(.largeTitle.weight(.semibold))
+                        }
+                        ForEach(0..<3) { number in
+                            Button {
+                                flagTapped(number)
+                            } label: {
+                                Image(countries[number])
+                                    .renderingMode(.original)
+                                    .clipShape(RoundedRectangle(cornerRadius: 35))
+                                    .shadow(radius: 23)
+                            }
                         }
                     }
+                    .frame(maxWidth: .infinity)
+                    .padding(.vertical, 40)
+                    .background(.ultraThinMaterial.blendMode(.darken))
+                    .clipShape(RoundedRectangle(cornerRadius: 50))
+                    Spacer()
+                    Spacer()
                 }
-                .frame(maxWidth: .infinity)
-                .padding(.vertical, 40)
-                .background(.ultraThinMaterial.blendMode(.darken))
-                .clipShape(RoundedRectangle(cornerRadius: 50))
-                Spacer()
-                Spacer()
+
                 Text("Score is \(score)")
                     .foregroundStyle(Color.accentColor)
                     .font(.title).bold()
@@ -66,28 +73,50 @@ struct ContentView: View {
         .alert(scoreTitle, isPresented: $showingScore) {
             Button("Continue", action: askQuestion)
         } message: {
-                Text("Your score is \(score)")
-            }
+            Text("Your score is \(score)")
         }
+        .alert("Game Over!", isPresented: $gameOver) {
+            Button("Reset?", action: resetGame)
+        } message: {
+            Text("Ending score is \(score)")
+        }
+    }
+    
     
     func flagTapped(_ number: Int) {
         if number == correctAnswer {
             scoreTitle = "Correct"
             score += 1
+            seenSoFarCountries.append(countries[number])
+            countries.remove(at: number)
         }
         else {
-            scoreTitle = "Wrong"
+            scoreTitle = "Wrong! This is actually \(countries[number])'s flag!"
         }
         
-        showingScore = true
+        turnCount += 1
+        
+        if turnCount < 8 {
+            showingScore = true
+        }
+        else {
+            gameOver = true
+        }
+
     }
     
     func askQuestion() {
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
     }
+    
+    func resetGame() {
+        turnCount = 0
+        score = 0
+        gameOver = false
+        countries.append(contentsOf: seenSoFarCountries)
+    }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
